5206461a3221776893b174e4698e79d487da1852
abrassel <abrassel@umd.edu>
2018-11-07 20:35:00 -0500

    completed week 9

--- week/9/part2.py
+++ week/9/part2.py
@@ -4,17 +4,29 @@
 # importing useful libraries -- feel free to add any others you find necessary
 import socket
 import hashlib
+import re
 
-host = ""   # IP address or URL
-port =      # port
+parser = re.compile("Find me the (\w+) hash of (\w+)")
+
+host = "142.93.117.193"   # IP address or URL
+port = 7331     # port
 
 # use these to connect to the service
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-# receive some data
-data = s.recv(1024)
-print(data)
+
+while True:
+    data = s.recv(1024)
+    is_match = parser.search(data)
+
+    if not is_match:
+        print data
+        break
+    hash_type, target = is_match.groups()
+
+    guess = getattr(hashlib, hash_type)(target).hexdigest()
+    s.send(guess + '\n')
 
 # close the connection
 s.close()
